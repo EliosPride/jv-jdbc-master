4e23453e15365c66d007d6d605938d07e7cb6665
Illia Sydorchuk <batonvagon@gmail.com>
2022-06-18 20:19:20 +0300

    implement properties

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -1,5 +1,11 @@
 package mate.jdbc.util;
 
+import mate.jdbc.exception.DataProcessingException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,13 +21,13 @@ public class ConnectionUtil {
     }
 
     public static Connection getConnection() {
+        File file = new File("src/main/resources/data.properties");
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "Aktersobaka1992");
+            dbProperties.load(new FileReader(file));
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service", dbProperties);
-        } catch (SQLException throwables) {
-            throw new RuntimeException("Can't create connection to DB", throwables);
+        } catch (SQLException | IOException e) {
+            throw new DataProcessingException("Can't create connection to DB", e);
         }
     }
 }
